77ecc692241fbdee9e7b2d0e50ffc5d21b1c2b6f
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2018-02-17 17:17:01 +0200

    make it concurrent

--- teams.go
+++ teams.go
@@ -3,14 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
+	"sync"
 )
 
 type Player struct {
-	Id        string `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Age       string `json:"age"`
+	Id    string   `json:"id"`
+	Name  string   `json:"name"`
+	Age   string   `json:"age"`
+	Teams []string `json:"teams"`
 }
 
 type Team struct {
@@ -18,6 +21,9 @@ type Team struct {
 	IsNational bool     `json:"isNational"`
 	Players    []Player `json:"players"`
 }
+
+var teams = []string{"Germany", "England", "France", "Spain", "Arsenal", "Chelsea", "Barcelona", "Real Madrid", "Manchester Utd", "FC Bayern Munich"}
+var roster map[string]Player
 
 func GetAttribute(dec *json.Decoder, attribute string) string {
 	for dec.More() {
@@ -31,19 +37,29 @@ func GetAttribute(dec *json.Decoder, attribute string) string {
 	return ""
 }
 
-func GetTeamData(dec *json.Decoder) (players []Player) {
-	for dec.More() {
-		token := NextToken(dec)
-		if token == "team" {
-			var team Team
-			err := dec.Decode(&team)
-			if err != nil {
-				panic(err)
-			}
-			return team.Players
-		}
+func GetTeamData(team string, dec *json.Decoder) {
+	// look for the players object
+	for token := NextToken(dec); token != "players"; {
+		token = NextToken(dec)
 	}
-	return
+	// parse start of the players object
+	NextToken(dec)
+	for dec.More() {
+		var player Player
+		err := dec.Decode(&player)
+		if err != nil {
+			panic(err)
+		}
+		if player.Teams == nil {
+			player.Teams = make([]string, 2)
+		}
+		player.Teams = []string{team}
+		if _, found := roster[player.Id]; found {
+			newTeams := append(roster[player.Id].Teams, team)
+			player.Teams = newTeams
+		}
+		roster[player.Id] = player
+	}
 }
 
 func NextToken(dec *json.Decoder) (token json.Token) {
@@ -54,42 +70,44 @@ func NextToken(dec *json.Decoder) (token json.Token) {
 	return
 }
 
-func main() {
-	teams := map[string][]Player{
-		"Germany":          []Player{},
-		"England":          []Player{},
-		"France":           []Player{},
-		"Spain":            []Player{},
-		"Arsenal":          []Player{},
-		"Chelsea":          []Player{},
-		"Barcelona":        []Player{},
-		"Real Madrid":      []Player{},
-		"Manchester Utd":   []Player{},
-		"FC Bayern Munich": []Player{},
+func GetURL(url string, ch chan io.ReadCloser) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
 	}
-	intId := 1
-	found := 0
-	for {
-		url := fmt.Sprintf("https://vintagemonster.onefootball.com/api/teams/en/%v.json", intId)
-		resp, err := http.Get(url)
-		if resp.StatusCode != 200 {
-			break
-		}
-		if err != nil {
-			panic(err)
-		}
-		dec := json.NewDecoder(resp.Body)
-		if err != nil {
-			panic(err)
-		}
-		if _, ok := teams[GetAttribute(dec, "name")]; ok {
-			fmt.Println(intId)
-			found++
-		}
-		if found == len(teams) {
-			break
-		}
-		intId++
+	if resp.StatusCode != 200 {
+		fmt.Println(url)
+		panic(err)
 	}
+	ch <- resp.Body
+}
 
+func ProcessJSONData(in chan io.ReadCloser, wg *sync.WaitGroup) {
+	defer wg.Done()
+	body := <-in
+	defer body.Close()
+	dec := json.NewDecoder(body)
+	teamName := GetAttribute(dec, "name")
+	for _, t := range teams {
+		if teamName == t {
+			GetTeamData(teamName, dec)
+		}
+	}
+}
+
+func main() {
+	httpData := make(chan io.ReadCloser, 10)
+	roster = make(map[string]Player)
+	var wg sync.WaitGroup
+	for id := 1; id < 100; id++ {
+		url := fmt.Sprintf("https://vintagemonster.onefootball.com/api/teams/en/%v.json", id)
+		go GetURL(url, httpData)
+		wg.Add(1)
+		go ProcessJSONData(httpData, &wg)
+	}
+	wg.Wait()
+	close(httpData)
+	for _, p := range roster {
+		fmt.Printf("%v; %v; %v.\n", p.Name, p.Age, strings.Join(p.Teams, ","))
+	}
 }
